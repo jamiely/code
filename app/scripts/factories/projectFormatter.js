387716f7931c50d21fac7e8da717de00f687dc7c
Jamie Ly <me@jamie.ly>
2014-03-20 22:00:45 -0400

    Added mainImage

--- app/scripts/factories/projectFormatter.js
+++ app/scripts/factories/projectFormatter.js
@@ -3,9 +3,14 @@
 angular.module('codeGalleryApp')
   .factory('ProjectFormatterFactory', function () {
     function transform(project) {
+      project.images = [];
       project.links.forEach(function(link) {
         if(link.type == 'article') {
           project.mainArticleLink = link;
+        }
+        if(link.type == 'image') {
+          project.images.push(link);
+          project.mainImage = link;
         }
       });
     }
