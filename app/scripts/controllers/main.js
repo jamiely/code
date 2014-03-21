0a5cf6b5513c6f4c7c8ba63526ceae0ec5e330fe
Jamie Ly <me@jamie.ly>
2014-03-21 16:15:15 -0400

    Moved controller out

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,8 +1,13 @@
 'use strict';
 
+(function() {
+
+var MainCtrl = function ($scope, projects) {
+  projects.index().then(function(p) {
+    $scope.projects = p;
+  });
+};
+
 angular.module('codeGalleryApp')
-  .controller('MainCtrl', ['$scope', 'ProjectService', function ($scope, projects) {
-    projects.index().then(function(p) {
-      $scope.projects = p;
-    });
-  }]);
+  .controller('MainCtrl', ['$scope', 'ProjectService', MainCtrl]);
+})();
